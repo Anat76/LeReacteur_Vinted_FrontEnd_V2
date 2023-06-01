35d1c995c803752db8b8a24f209d8db37083b042
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-06-01 12:12:20 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,52 @@
-import axios from "axios";
-import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
+import { Navigate, useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Login = ({ cookieToken }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
   return (
     <section>
-      <h1>S'inscrire</h1>
       <form
-        onSubmit={(event) => {
+        onSubmit={async (event) => {
           event.preventDefault();
+          try {
+            const result = await axios.post(
+              "http://localhost:3000/user/login",
+              {
+                email: email,
+                password: password,
+              }
+            );
+            if (result.data.token) {
+              cookieToken(result.data.token);
+              navigate("/");
+            }
+          } catch (error) {
+            console.log(error.message);
+          }
         }}
-      ></form>
+      >
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="votre mot de passe"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          value={password}
+        />
+        <button type="submit">Se connecter</button>
+      </form>
     </section>
   );
 };
