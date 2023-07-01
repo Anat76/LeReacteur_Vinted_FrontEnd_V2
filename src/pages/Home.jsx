fef3d513c36ee08df52b70884d282111baabf601
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-07-01 22:18:17 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import OfferCard from "../components/OfferCard";
+import hero from "../assets/img/hero.jpg";
+import { Link } from "react-router-dom";
 
 const Home = ({ search, sort }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -27,12 +29,20 @@ const Home = ({ search, sort }) => {
   return isLoading ? (
     <h1>Loading ...</h1>
   ) : (
-    <>
-      <h1>Home</h1>
-      {dataOffers.offer.map((offer) => {
+    <main>
+      <div className="hero">
+        <img src={hero} alt="" />
+        <div>
+          <h1>Prêts à faire du tri dans vos placards ?</h1>
+          <Link to={"/publish"}>
+            <button>Commencer à vendre</button>
+          </Link>
+        </div>
+      </div>
+      {/* {dataOffers.offer.map((offer) => {
         return <OfferCard key={offer._id} offer={offer} />;
-      })}
-    </>
+      })} */}
+    </main>
   );
 };
 
